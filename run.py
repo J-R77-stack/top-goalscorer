9b0f06a5dbf97e8473da5e4a62a07c9e57f7e550
J-R77-stack <johnnyramsay81@gmail.com>
2021-10-01 15:09:39 +0000

    practice

--- run.py
+++ run.py
@@ -1,43 +1,75 @@
-import gspread
-from google.oauth2.service_account import Credentials
+# ---- Global Variables
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+# Game board
+board = ["-","-","-",
+         "-","-","-",
+         "-","-","-",]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('top_goalscorer')
+# If game is still going
+game_still_going = True
 
-def get_goalscorer_data():
-    """
-    Get goal scorer row number from user
-    """
-    print("Please enter row numbers from goalscorer sheet.")
-    print("Data should be a numbers between 2 to 140, separated by commas")
-    print("Example: 2, 10, 41 etc\n")
+# Who won? Or tie?
+winner = None
 
-    data_str = input("Enter your row number here:")
-    
-    goalscorer_data = data_str.split(",")
-    validate_data(goalscorer_data)
+# Whos turn is it
+current_player = "X"
 
-def validate_data(values):
-    """
-    Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int, 
-    and only allows 5 rows of data at a time.
-    """
-    try:
-        [int(value)for value in values]
-        if len(values) >= 6:
-            raise ValueError(
-                f"Only five or less rows of data allowed, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"invalid entry: {e} please enter 5 or less row numbers")        
+def display_board():
+  print(" | " + board[0] + " | " + board[1] + " | " + board[2]+ " | ")
+  print(" | " + board[3] + " | " + board[4] + " | " + board[5]+ " | ")
+  print(" | " + board[6] + " | " + board[7] + " | " + board[8]+ " | ")
 
-get_goalscorer_data()      
+def play_game():
+  
+  # Display initial board
+  display_board() 
+
+  while game_still_going:
+
+      handle_turn(current_player) 
+
+      check_if_game_over()
+
+      flip_player()
+
+  # The game has ended
+  
+def handle_turn(player):
+  position = input("choose a position from 1-9: ")
+  position = int(position) - 1
+
+  board[position] = "X"
+  display_board()
+
+
+
+def check_if_game_over():
+    check_if_win()
+    check_if_tie()
+
+def check_if_win():
+    # check rows
+    # check colums
+    # check diagonals
+    return
+
+def check_if_tie():
+    return
+
+def flip_player():
+    return
+
+play_game()       
+
+
+# board
+# display
+# play game
+# handle turn
+# check win
+  # check rows
+  # check columns
+  # check diagonals
+# check tie
+# flip player  
+
